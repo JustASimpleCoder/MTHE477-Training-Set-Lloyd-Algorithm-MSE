87cd4739c908bf0e07b36e6a7b5da4290e60dccd
Jacob Cohen <42702125+JustASimpleCoder@users.noreply.github.com>
2022-03-06 03:29:04 -0500

    Add files via upload

--- AlgorithmRate2.m
+++ AlgorithmRate2.m
@@ -7,47 +7,44 @@ rng('default')
 trainingSet = randn(1,5000);
 % Apply Lloyd algorithm for training data and MSE:
 
+
 %Step 1: (inputs)
 epsilon = 0.001;
 m = 1;
 % MSE with quantizer rate R = 2, recalling R = log_{2}(N), then  N = 4
-% given the training set we approximate the distribution of X by
-% a vector of the probabilities
-sourceDistApprx = [];
-for i = 1:5000
-    sourceDistApprx(i) = (1/i);
-end
-% For MSE optimal, initialize codebook using Lloyd-Max conidtions
-% first construct 4-level quantizer with c being the expectation of X
-% given X > 0 where X ~ sourceDistApprx, since it is discreet we use
-% discrete expectation to approximate the continous expectation:
-c = 0;
+% given the training set we approximate the distribution of X 
 for i = 1:5000 
-   c = c+ 2*trainingSet(i)*sourceDistApprx(i);
+   c = c + 2*trainingSet(i)*(1/5000);
 end
+
 % thus the inital codebook 1 is
-codeBookRate1_1 = [-c, -c/2 ,c/2, c/2];
+initialCodeBook = [-c, -c/2 ,c/2, c];
 % with corresponding bins, R_1 = (-inf, (-3/2)c] and R_2 = ((-3/2)c, 0]
 % R_3 = (0, (3/2)c] and r_4 = ((3/2)c, inf)
 % with the 2-level quantizer Q(x) = c if x >  0 and Q(x) = -c if x<=0
 % with distortion
 
-% step 2: 
-[y1, y2, y3, y4] = partitionCodebook(c,1);
-% Step3:
-while ((meanDistortion(c,m) - meanDistortion(c,m+1))/(meanDistortion(c,m))) < epsilon
-    [y1, y2, y3, y4] = partitionCodebook(c,m);
+
+[c1_m, c2_m, c3_m, c4_m] =  partitionCodebook(initialCodeBook, trainingSet, m);
+while ((meanDistortion([c1_m, c2_m, c3_m, c4_m], trainingSet,m) - meanDistortion([c1_m, c2_m, c3_m, c4_m], trainingSet, m+1))/(meanDistortion([c1_m, c2_m, c3_m, c4_m], trainingSet, m))) < epsilon
+    [c1_m, c2_m, c3_m, c4_m] = partitionCodebook([-c, -c/2 ,c/2, c/2],m);
     m = m + 1;
 end
 disp('The codebook yielding an approximation to the optimal code is: ')
-disp([y1, y2, y3, y4])
+disp([c1_m, c2_m, c3_m, c4_m])
+disp(meanDistortion([c1_m, c2_m, c3_m, c4_m], trainingSet,m))
 
 
 
 % fucnction that partitions the codeboopk for the m-th codeword, given
 % the initial codebook [-c, c] (follows from step 2 of LLoyds algorithm)
-function [y1_m, y2_m, y3_m, y4_m]  = partitionCodebook(c,m)
-    trainingSet = randn(1,5000);
+function [y1_m, y2_m, y3_m, y4_m]  = partitionCodebook(codeBook, tset, m)
+    trainingSet = tset;
+    c1 = codeBook(1);
+    c2 = codeBook(2);
+    c3 = codeBook(3);
+    c4 = codeBook(4);
+    
     %initialize bins
     bin1 = [];
     bin2 = [];
@@ -59,16 +56,16 @@ function [y1_m, y2_m, y3_m, y4_m]  = partitionCodebook(c,m)
     l = 1;
     % Generate partitions using the NNC
     for i = 1:5000
-        if (abs(trainingSet(i) - c^m) <= abs(trainingSet(i) + c^m)) && (abs(trainingSet(i) - c^m) <= abs(trainingSet(i) + (c/2)^m)) && (abs(trainingSet(i) - c^m) <= abs(trainingSet(i) - (c/2)^m))
+        if (abs(trainingSet(i) - c1^m) <= abs(trainingSet(i) - c2^m)) && abs(trainingSet(i) - c1^m) <= abs(trainingSet(i) - c3^m) && abs(trainingSet(i) - c1^m) <= abs(trainingSet(i) - c4^m)
             bin1(j) = trainingSet(i);
             j = j + 1;
-        elseif (abs(trainingSet(i) - (c/2)^m) <= abs(trainingSet(i) + c^m)) && (abs(trainingSet(i) - (c/2)^m) <= abs(trainingSet(i) + (c/2)^m)) && (abs(trainingSet(i) - (c/2)^m) <= abs(trainingSet(i) - (c)^m))
+        elseif abs(trainingSet(i) - c2^m) <= abs(trainingSet(i) - c1^m) && abs(trainingSet(i) - c2^m) <= abs(trainingSet(i) - c3^m) && abs(trainingSet(i) - c2^m) <= abs(trainingSet(i) - c4^m)
             bin2(s) = trainingSet(i);
             s = s + 1;
-        elseif (abs(trainingSet(i) + (c/2)^m) <= abs(trainingSet(i) + c^m)) && (abs(trainingSet(i) + (c/2)^m) <= abs(trainingSet(i) - (c/2)^m)) && (abs(trainingSet(i) + (c/2)^m) <= abs(trainingSet(i) - (c)^m))
+        elseif abs(trainingSet(i) - c3^m) <= abs(trainingSet(i) - c1^m) && abs(trainingSet(i) - c3^m) <= abs(trainingSet(i) - c2^m) && abs(trainingSet(i) - c3^m) <= abs(trainingSet(i) - c4^m)
             bin3(t) = trainingSet(i);
             t = t + 1;     
-        elseif (abs(trainingSet(i) + (c)^m) <= abs(trainingSet(i) + (c/2)^m)) && (abs(trainingSet(i) + (c)^m) <= abs(trainingSet(i) - (c/2)^m)) && (abs(trainingSet(i) + (c)^m) <= abs(trainingSet(i) - (c)^m))
+        elseif abs(trainingSet(i) - c4^m) <= abs(trainingSet(i) - c1^m) && abs(trainingSet(i) - c4^m) <= abs(trainingSet(i) - c2^m) && abs(trainingSet(i) - c4^m) <= abs(trainingSet(i) - c3^m)
             bin4(l) = trainingSet(i);
             l = l + 1;
         end
@@ -102,23 +99,28 @@ function [y1_m, y2_m, y3_m, y4_m]  = partitionCodebook(c,m)
         sum_bin4 = sum_bin4 + bin4(i);
     end
     y4_m = (1/(sz_bin4(2)))*(sum_bin4);
-
 end
   
 % computes the mean distortion after the m-th iteration of the code
 % (follows from step 3 of LLoyds algorithm)
-function D_m = meanDistortion(c, m)
-    trainingSet = randn(1,5000);
+function D_m = meanDistortion(codeBook, tset, m)
+    trainingSet = tset;
+    c1 = codeBook(1);
+    c2 = codeBook(2);
+    c3 = codeBook(3);
+    c4 = codeBook(4);
+
+
     sum_distortion = 0;
     for i = 1:5000
-        if trainingSet(i) > c
-            sum_distortion = sum_distortion + (trainingSet(i) - c^m);
-        elseif trainingSet(i) <= c && trainingSet(i) > c/2
-            sum_distortion = sum_distortion + (trainingSet(i) - (c/2)^m);
-        elseif trainingSet(i) >= -c && trainingSet(i) <= -c/2
-            sum_distortion = sum_distortion + (trainingSet(i) + (c/2)^m);
-        elseif trainingSet(i) <= -c 
-            sum_distortion = sum_distortion + (trainingSet(i) + (c)^m);
+        if trainingSet(i) > c4
+            sum_distortion = sum_distortion + (trainingSet(i) - c1^m)^2;
+        elseif trainingSet(i) <= c4 && trainingSet(i) > c3
+            sum_distortion = sum_distortion + (trainingSet(i) - (c2)^m)^2;
+        elseif trainingSet(i) <= c3 && trainingSet(i) <= c2
+            sum_distortion = sum_distortion + (trainingSet(i) - (c3)^m)^2;
+        elseif trainingSet(i) <= c1 
+            sum_distortion = sum_distortion + (trainingSet(i) - (c4)^m)^2;
         end
     end
     D_m = (1/5000)*sum_distortion;
